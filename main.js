67f4090931e46c9cdde9b847a299c2030bd26bc0
GabrielTVG <tvgabriel@yahoo.com>
2020-11-11 19:38:08 +0200

    Curs 1 introducere html css css

---
+++ main.js
@@ -0,0 +1,16 @@
+var name = "Paul";
+var ammount = 23;
+var knowJavascript = true;
+var fruits = ["apple","lemon","orange"];
+
+function add(){
+    return 2 + 3;
+}
+
+var person = {
+    height: 2,
+    canWalk : function(){
+        console.log("Yes, I can walk");
+    },
+    name: "Julia"
+}
